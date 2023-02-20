291ffd0946ccd69d9695f55b80795ecbb626cd31
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-02-20 10:59:23 +0600

    Re organize the routes

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\GoogleAuthController;
+
 use App\Http\Controllers\HomeController;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 
 Route::get('/auth/google', [GoogleAuthController::class, 'redirect'])->name('google.auth');
 Route::get('/auth/google/call-back', [GoogleAuthController::class, 'callbackGoogle']);
+*/
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
-
-// Route::get('/', function () {
-//     return view('user.index');
-// });
 
 Route::get('/shop', function () {
     return view('user.shop');
