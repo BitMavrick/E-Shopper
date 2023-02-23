190ab42137445e8cd4ac7bb0512760d2d7c1567b
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-02-23 17:24:57 +0600

    Organized the navbar options

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -116,17 +116,10 @@
                     </button>
                     <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
                         <div class="navbar-nav mr-auto py-0">
-                            <a href="index.html" class="nav-item nav-link active">Home</a>
-                            <a href="shop.html" class="nav-item nav-link">Shop</a>
-                            <a href="detail.html" class="nav-item nav-link">Shop Detail</a>
-                            <div class="nav-item dropdown">
-                                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Pages</a>
-                                <div class="dropdown-menu rounded-0 m-0">
-                                    <a href="cart.html" class="dropdown-item">Shopping Cart</a>
-                                    <a href="checkout.html" class="dropdown-item">Checkout</a>
-                                </div>
-                            </div>
-                            <a href="contact.html" class="nav-item nav-link">Contact</a>
+                            <a href="index.html" class="nav-item nav-link">Home</a>
+                            <a href="detail.html" class="nav-item nav-link">Explore</a>
+                            <a href="shop.html" class="nav-item nav-link active">Your Feed</a>
+                            <a href="contact.html" class="nav-item nav-link">Contact Us</a>
                         </div>
                         <div class="navbar-nav ml-auto py-0">
                             @if(Auth::user())
