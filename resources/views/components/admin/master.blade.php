1e70c61e83856a6f6439db620e092b98dd763c90
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-02-26 18:06:55 +0600

    Blading up the admin panel

---
+++ resources/views/components/admin/master.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Reporter | Admin</title>
+    <!-- base:css -->
+    <link rel="stylesheet" href="admin/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="admin/vendors/feather/feather.css">
+    <link rel="stylesheet" href="admin/vendors/base/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <link rel="stylesheet" href="admin/vendors/flag-icon-css/css/flag-icon.min.css" />
+    <link rel="stylesheet" href="admin/vendors/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="admin/vendors/jquery-bar-rating/fontawesome-stars-o.css">
+    <link rel="stylesheet" href="admin/vendors/jquery-bar-rating/fontawesome-stars.css">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="admin/css/style.css">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="admin/images/favicon.png" />
+</head>
+
+<body>
+    {{ $slot }}
+
+    <!-- container-scroller -->
+
+    <!-- base:js -->
+    <script src="admin/vendors/base/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="admin/js/off-canvas.js"></script>
+    <script src="admin/js/hoverable-collapse.js"></script>
+    <script src="admin/js/template.js"></script>
+    <!-- endinject -->
+    <!-- plugin js for this page -->
+    <script src="admin/vendors/chart.js/Chart.min.js"></script>
+    <script src="admin/vendors/jquery-bar-rating/jquery.barrating.min.js"></script>
+    <!-- End plugin js for this page -->
+    <!-- Custom js for this page-->
+    <script src="admin/js/dashboard.js"></script>
+    <!-- End custom js for this page-->
+</body>
