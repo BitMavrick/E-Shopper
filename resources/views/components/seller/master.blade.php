465445e11baf21e83fbd8655153f3a6bf2a71287
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-03-14 20:27:53 +0600

    start creating seller dashboard

---
+++ resources/views/components/seller/master.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>{{ $title ?? 'Admin | E-Shopper'}}</title>
+
+    <!-- CKeditor -->
+    <script src="https://cdn.ckeditor.com/ckeditor5/36.0.1/classic/ckeditor.js"></script>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- base:css -->
+    <link rel="stylesheet" href="/admin/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="/admin/vendors/feather/feather.css">
+    <link rel="stylesheet" href="/admin/vendors/base/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <link rel="stylesheet" href="/admin/vendors/flag-icon-css/css/flag-icon.min.css" />
+    <link rel="stylesheet" href="/admin/vendors/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/admin/vendors/jquery-bar-rating/fontawesome-stars-o.css">
+    <link rel="stylesheet" href="/admin/vendors/jquery-bar-rating/fontawesome-stars.css">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="/admin/css/style.css">
+    <!-- endinject -->
+    <link href="/user/img/icon.svg" rel="icon" />
+</head>
+
+<body>
+
+    <div class="container-scroller">
+        <!-- partial:partials/_navbar.html -->
+        <x-admin.partials.navbar />
+        <!-- partial -->
+        <div class="container-fluid page-body-wrapper">
+            <!-- partial:partials/_sidebar.html -->
+            <x-admin.partials.sidebar />
+            <!-- partial -->
+            <div class="main-panel">
+                {{ $slot }}
+                <!-- content-wrapper ends -->
+                <x-admin.partials.footer />
+                <!-- partial:partials/_footer.html -->
+                <!-- partial -->
+            </div>
+            <!-- main-panel ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+    <!-- base:js -->
+    <script src="/admin/vendors/base/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="/admin/js/off-canvas.js"></script>
+    <script src="/admin/js/hoverable-collapse.js"></script>
+    <script src="/admin/js/template.js"></script>
+    <!-- endinject -->
+    <!-- plugin js for this page -->
+    <script src="/admin/vendors/chart.js/Chart.min.js"></script>
+    <script src="/admin/vendors/jquery-bar-rating/jquery.barrating.min.js"></script>
+    <!-- End plugin js for this page -->
+    <!-- Custom js for this page-->
+    <script src="/admin/js/dashboard.js"></script>
+    <!-- End custom js for this page-->
+</body>
---
+++ resources/views/components/seller/partials/footer.blade.php
@@ -0,0 +1,8 @@
+ <footer class="footer">
+     <div class="d-sm-flex justify-content-center justify-content-sm-between">
+         <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright ©
+             <a href="https://github.com/TheAlter71" target="_blank">Mehedi Hasan</a> 2020</span>
+
+     </div>
+     <span class="text-muted d-block text-center text-sm-left d-sm-inline-block mt-2">Distributed By: <a href="https://www.themewagon.com/" target="_blank">ThemeWagon</a></span>
+ </footer>
